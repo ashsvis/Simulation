43f192a5890990ff71afea7e2ce28aaabde8f359
Александр Шаповалов <ashsvis@gmail.com>
2025-10-04 16:52:57 +0300

    Удаление последнего входа для OR и XOR

--- Simulator/Model/Logic/OR.cs
+++ Simulator/Model/Logic/OR.cs
@@ -10,84 +10,151 @@ namespace Simulator.Model.Logic
         }
     }
 
-    public class OR3 : CommonLogic, IAddInput { public OR3() : base(LogicFunction.Or, 3) { }
+    public class OR3 : CommonLogic, IAddInput, IRemoveInput { public OR3() : base(LogicFunction.Or, 3) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class OR4 : CommonLogic, IAddInput { public OR4() : base(LogicFunction.Or, 4) { }
+    public class OR4 : CommonLogic, IAddInput, IRemoveInput { public OR4() : base(LogicFunction.Or, 4) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class OR5 : CommonLogic, IAddInput { public OR5() : base(LogicFunction.Or, 5) { }
+    public class OR5 : CommonLogic, IAddInput, IRemoveInput { public OR5() : base(LogicFunction.Or, 5) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class OR6 : CommonLogic, IAddInput { public OR6() : base(LogicFunction.Or, 6) { }
+    public class OR6 : CommonLogic, IAddInput, IRemoveInput { public OR6() : base(LogicFunction.Or, 6) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class OR7 : CommonLogic, IAddInput { public OR7() : base(LogicFunction.Or, 7) { }
+    public class OR7 : CommonLogic, IAddInput, IRemoveInput { public OR7() : base(LogicFunction.Or, 7) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class OR8 : CommonLogic { public OR8() : base(LogicFunction.Or, 8) { } }
+    public class OR8 : CommonLogic, IRemoveInput { public OR8() : base(LogicFunction.Or, 8) { }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
+    }
 
     public static class OrHelper
     {
-        public static void AddInputAt(this Model.Logic.OR and, Element element)
+        public static void AddInputAt(this Model.Logic.OR or, Element element)
         {
-            var and3 = new Model.Logic.OR3();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or3 = new Model.Logic.OR3();
+            Project.UpdateElementAndFunction(or, element, or3);
         }
 
-        public static void AddInputAt(this Model.Logic.OR3 and, Element element)
+        public static void AddInputAt(this Model.Logic.OR3 or3, Element element)
         {
-            var and3 = new Model.Logic.OR4();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or4 = new Model.Logic.OR4();
+            Project.UpdateElementAndFunction(or3, element, or4);
         }
 
-        public static void AddInputAt(this Model.Logic.OR4 and, Element element)
+        public static void RemoveInputAt(this Model.Logic.OR3 or3, Element element)
         {
-            var and3 = new Model.Logic.OR5();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or2 = new Model.Logic.OR();
+            Project.UpdateElementAndFunction(or3, element, or2);
         }
 
-        public static void AddInputAt(this Model.Logic.OR5 and, Element element)
+        public static void AddInputAt(this Model.Logic.OR4 or4, Element element)
         {
-            var and3 = new Model.Logic.OR6();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or5 = new Model.Logic.OR5();
+            Project.UpdateElementAndFunction(or4, element, or5);
         }
 
-        public static void AddInputAt(this Model.Logic.OR6 and, Element element)
+        public static void RemoveInputAt(this Model.Logic.OR4 or4, Element element)
         {
-            var and3 = new Model.Logic.OR7();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or3 = new Model.Logic.OR3();
+            Project.UpdateElementAndFunction(or4, element, or3);
         }
 
-        public static void AddInputAt(this Model.Logic.OR7 and, Element element)
+        public static void AddInputAt(this Model.Logic.OR5 or5, Element element)
         {
-            var and3 = new Model.Logic.OR8();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var or6 = new Model.Logic.OR6();
+            Project.UpdateElementAndFunction(or5, element, or6);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.OR5 or5, Element element)
+        {
+            var or4 = new Model.Logic.OR4();
+            Project.UpdateElementAndFunction(or5, element, or4);
+        }
+
+        public static void AddInputAt(this Model.Logic.OR6 or6, Element element)
+        {
+            var or7 = new Model.Logic.OR7();
+            Project.UpdateElementAndFunction(or6, element, or7);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.OR6 or6, Element element)
+        {
+            var or5 = new Model.Logic.OR5();
+            Project.UpdateElementAndFunction(or6, element, or5);
+        }
+
+        public static void AddInputAt(this Model.Logic.OR7 or7, Element element)
+        {
+            var or8 = new Model.Logic.OR8();
+            Project.UpdateElementAndFunction(or7, element, or8);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.OR7 or7, Element element)
+        {
+            var or6 = new Model.Logic.OR6();
+            Project.UpdateElementAndFunction(or7, element, or6);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.OR8 or8, Element element)
+        {
+            var or7 = new Model.Logic.OR7();
+            Project.UpdateElementAndFunction(or8, element, or7);
         }
 
     }
--- Simulator/Model/Logic/XOR.cs
+++ Simulator/Model/Logic/XOR.cs
@@ -10,84 +10,151 @@ namespace Simulator.Model.Logic
         }
     }
 
-    public class XOR3 : CommonLogic, IAddInput { public XOR3() : base(LogicFunction.Xor, 3) { }
+    public class XOR3 : CommonLogic, IAddInput, IRemoveInput { public XOR3() : base(LogicFunction.Xor, 3) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class XOR4 : CommonLogic, IAddInput { public XOR4() : base(LogicFunction.Xor, 4) { }
+    public class XOR4 : CommonLogic, IAddInput, IRemoveInput { public XOR4() : base(LogicFunction.Xor, 4) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class XOR5 : CommonLogic, IAddInput { public XOR5() : base(LogicFunction.Xor, 5) { }
+    public class XOR5 : CommonLogic, IAddInput, IRemoveInput { public XOR5() : base(LogicFunction.Xor, 5) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class XOR6 : CommonLogic, IAddInput { public XOR6() : base(LogicFunction.Xor, 6) { }
+    public class XOR6 : CommonLogic, IAddInput, IRemoveInput { public XOR6() : base(LogicFunction.Xor, 6) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class XOR7 : CommonLogic, IAddInput { public XOR7() : base(LogicFunction.Xor, 7) { }
+    public class XOR7 : CommonLogic, IAddInput, IRemoveInput { public XOR7() : base(LogicFunction.Xor, 7) { }
 
         public void AddInput(Element element)
         {
             this.AddInputAt(element);
         }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
     }
 
-    public class XOR8 : CommonLogic { public XOR8() : base(LogicFunction.Xor, 8) { } }
+    public class XOR8 : CommonLogic, IRemoveInput { public XOR8() : base(LogicFunction.Xor, 8) { }
+
+        public void RemoveInput(Element element)
+        {
+            this.RemoveInputAt(element);
+        }
+    }
 
     public static class XorHelper
     {
-        public static void AddInputAt(this Model.Logic.XOR and, Element element)
+        public static void AddInputAt(this Model.Logic.XOR xor2, Element element)
         {
-            var and3 = new Model.Logic.XOR3();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor3 = new Model.Logic.XOR3();
+            Project.UpdateElementAndFunction(xor2, element, xor3);
         }
 
-        public static void AddInputAt(this Model.Logic.XOR3 and, Element element)
+        public static void AddInputAt(this Model.Logic.XOR3 xor3, Element element)
         {
-            var and3 = new Model.Logic.XOR4();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor4 = new Model.Logic.XOR4();
+            Project.UpdateElementAndFunction(xor3, element, xor4);
         }
 
-        public static void AddInputAt(this Model.Logic.XOR4 and, Element element)
+        public static void RemoveInputAt(this Model.Logic.XOR3 xor3, Element element)
         {
-            var and3 = new Model.Logic.XOR5();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor2 = new Model.Logic.XOR();
+            Project.UpdateElementAndFunction(xor3, element, xor2);
         }
 
-        public static void AddInputAt(this Model.Logic.XOR5 and, Element element)
+        public static void AddInputAt(this Model.Logic.XOR4 xor4, Element element)
         {
-            var and3 = new Model.Logic.XOR6();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor5 = new Model.Logic.XOR5();
+            Project.UpdateElementAndFunction(xor4, element, xor5);
         }
 
-        public static void AddInputAt(this Model.Logic.XOR6 and, Element element)
+        public static void RemoveInputAt(this Model.Logic.XOR4 xor4, Element element)
         {
-            var and3 = new Model.Logic.XOR7();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor3 = new Model.Logic.XOR3();
+            Project.UpdateElementAndFunction(xor4, element, xor3);
         }
 
-        public static void AddInputAt(this Model.Logic.XOR7 and, Element element)
+        public static void AddInputAt(this Model.Logic.XOR5 xor5, Element element)
         {
-            var and3 = new Model.Logic.XOR8();
-            Project.UpdateElementAndFunction(and, element, and3);
+            var xor6 = new Model.Logic.XOR6();
+            Project.UpdateElementAndFunction(xor5, element, xor6);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.XOR5 xor5, Element element)
+        {
+            var xor4 = new Model.Logic.XOR4();
+            Project.UpdateElementAndFunction(xor5, element, xor4);
+        }
+
+        public static void AddInputAt(this Model.Logic.XOR6 xor6, Element element)
+        {
+            var xor7 = new Model.Logic.XOR7();
+            Project.UpdateElementAndFunction(xor6, element, xor7);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.XOR6 xor6, Element element)
+        {
+            var xor5 = new Model.Logic.XOR5();
+            Project.UpdateElementAndFunction(xor6, element, xor5);
+        }
+
+        public static void AddInputAt(this Model.Logic.XOR7 xor7, Element element)
+        {
+            var xor8 = new Model.Logic.XOR8();
+            Project.UpdateElementAndFunction(xor7, element, xor8);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.XOR7 xor7, Element element)
+        {
+            var xor6 = new Model.Logic.XOR6();
+            Project.UpdateElementAndFunction(xor7, element, xor6);
+        }
+
+        public static void RemoveInputAt(this Model.Logic.XOR8 xor8, Element element)
+        {
+            var xor7 = new Model.Logic.XOR7();
+            Project.UpdateElementAndFunction(xor8, element, xor7);
         }
 
     }
